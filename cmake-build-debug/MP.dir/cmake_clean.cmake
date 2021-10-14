a326881fc981f9e3a5a01561ff994270b5ffa48e
Benjamin Carlson <bcarlson336@gmail.com>
2021-10-13 21:10:20 -0600

    Merged the-warrior into main

---
+++ cmake-build-debug/MP.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MP.dir/Car.cpp.o"
+  "CMakeFiles/MP.dir/MPEngine.cpp.o"
+  "CMakeFiles/MP.dir/main.cpp.o"
+  "CMakeFiles/MP.dir/the_warrior/TheWarrior.cpp.o"
+  "MP"
+  "MP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
